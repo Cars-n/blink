d26cb260c3a2ba82e7875a5a92c1ed4f6cfb1ad3
Cornelius Treadway Pavlic <43487455+Awesomecorn@users.noreply.github.com>
2024-02-07 15:01:47 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+let player;
+
+//Boxes to test collsions on
+let box;
+let box2;
+let box3;
+
+function setup() {
+	new Canvas();
+
+	// new Player 
+	player = new Sprite(30, 24, 64, 64);
+	setupPlayer();
+	playerMovement = new MovementController(player,3,true,true);
+
+
+	//Adding in for collsion testing
+	player.rotationLock = true;
+	world.gravity.y = 15;
+	box = new Sprite();
+	box2 = new Sprite(100,100,100,100);
+	box3 = new Sprite(-10,-10,-10,-10);
+
+	box.collider = 'kinematic';
+	box2.collider = 'kinematic';
+	box3.collider = 'kinematic';
+}
+
+function draw() {
+	clear();
+	playerMovement.handleMovement();
+
+	//Setting the debug on for sprite
+    player.debug = true;
+}
