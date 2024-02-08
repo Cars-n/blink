7fc09ace08344a1eac43f61f3a998568e850ce45
Chris-G32 <cgruska@kent.edu>
2024-02-07 20:03:25 -0500

    Updated controller to be velocity based

--- src/js/MovementController.js
+++ src/js/MovementController.js
@@ -1,23 +1,18 @@
+//Class for moving any sprite, whether through move up
 class MovementController {
-    constructor(target,moveSpeed=5,wasdControllable=false) {
+    //takes a sprite as a target to control
+    constructor(target, moveSpeed = 5) {
         this.target = target;
-        this.moveSpeed=moveSpeed;
-		this.wasdControllable=wasdControllable;
+        this.moveSpeed = moveSpeed;
     }
-
+    // Call this for sprites controllable by the player
     handleInput() {
-		if(this.wasdControllable){
-			this.handleMovement();
-		}
-    }//
-
-	handleMovement(){
-		this.target.velocity=new p5.Vector(0,0);		
-		// this.target.velocity=new p5.Vector(0,1)
+        this.target.velocity = new p5.Vector(0, 0);
+        // this.target.velocity=new p5.Vector(0,1)
         if (keyIsDown(87) || keyIsDown(UP_ARROW)) {
             this.moveUp();
         }
-		else if (keyIsDown(83) || keyIsDown(DOWN_ARROW)) {
+        else if (keyIsDown(83) || keyIsDown(DOWN_ARROW)) {
             this.moveDown();
         }
         if (keyIsDown(65) || keyIsDown(LEFT_ARROW)) {
@@ -26,23 +21,29 @@ class MovementController {
         else if (keyIsDown(68) || keyIsDown(RIGHT_ARROW)) {
             this.moveRight();
         }
-		//Normalize direction vec
-		this.target.velocity.normalize();
-		//Multiply magnitude of the 
-		this.target.velocity.setMag(this.moveSpeed);
-	}
+
+        this.handleMovement();
+
+    }
+    //Handles setting the proper velocity of the sprite
+    handleMovement() {
+        //Normalize direction vec
+        this.target.velocity.normalize();
+        //Multiply magnitude of the 
+        this.target.velocity.setMag(this.moveSpeed);
+    }
 
     //Movement functions
-	moveUp(){
-        this.target.velocity.y=-1;
+    moveUp() {
+        this.target.velocity.y = -1;
     }
-    moveDown(){
-        this.target.velocity.y=1; //Adding moves down in screen coords
+    moveDown() {
+        this.target.velocity.y = 1; //Adding moves down in screen coords
     }
-    moveRight(){
-        this.target.velocity.x=1;
+    moveRight() {
+        this.target.velocity.x = 1;
     }
-    moveLeft(){
-        this.target.velocity.x=-1;
+    moveLeft() {
+        this.target.velocity.x = -1;
     }
 }
