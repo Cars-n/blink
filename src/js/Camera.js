87d923d58b12afbcca824e77a78d299ff4e30ba7
Elliott Hager <83028849+eHag-FRU@users.noreply.github.com>
2024-02-14 19:43:31 -0500

    Added in the canvas resize for fullscreen

--- src/js/Camera.js
+++ src/js/Camera.js
@@ -1,4 +1,19 @@
+/**
+ * File dealing with camera and player view functionality
+ */
+
+//Adds an event listener to the window for resizing of
+//the browser window
+window.addEventListener("resize", canvasResize);
+
 function makeCameraFollowPlayer(){
     camera.x = player.x;
 	camera.y = player.y;
+}
+
+/**
+ * Function to resize the canvas based on changing window sizes
+ */
+function canvasResize() {
+    resizeCanvas();
 }
