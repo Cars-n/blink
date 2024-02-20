68e9bb6efe72a230a3bd3807227879e56ac2b1e7
SpeedyEggbert <126017385+SpeedyEggbert@users.noreply.github.com>
2024-02-19 19:08:44 -0500

    Updated the enemy class and added a class to allow for static enemy data

--- src/js/enemy.js
+++ src/js/enemy.js
@@ -1,30 +1,57 @@
-//Class for enemeis, unused at the moment but should be used and updated in the future
-class basicEnemey{
-    constructor(enemy_id, health){
+//Class for data of enemies
+//enemy_id: Specifies what type of enemy they are
+//ai_type: Specifies what type of ai the enemy has, all will probably have a unique one
+//name: Name of the enemy
+//health: the max hp
+//width: width of enemy
+class enemyData{
+    constructor(enemy_id, ai_type, name, health, height, width, dia){
         this.enemy_id = enemy_id;
+        this.ai_type = ai_type;
+        this.name = name;
         this.health = health;
+        this.height = height;
+        this.width = width;
+        this.dia = dia;
+    }
+}
+
+//Class used for data of currently spawned eneimes
+//enemy_id: Identifier of the enemy
+//ai_type: Specifies what type of ai the enemy has
+//name: Name of the enemy
+//health: Current hp of the enemy
+//width: width of the enemy
+//hight: hight of the enemy
+//locX: X Spawn cordinate of the enemy
+//locY: Y Spawn coridnate of the enemy
+class Enemy{
+    constructor(enemy_id){
+        this.enemy_id = enemy_id;
+        this.name = staticEnemyList[enemy_id].name;
+        this.health = staticEnemyList[enemy_id].health;
+        this.enemySprite = new Sprite(400, 400, 25, 25);
+        this.enemySprite.diameter = staticEnemyList[enemy_id].dia;
+        setObjectCollider(this.enemySprite, spriteTypes.ENEMY, true);
     }
 }
 
 //Spawns an enemy in default location, used mostly for debugging
-function spawnEnemy(){
-    let temp = new Sprite(400, 400, 25, 25);
-    temp.diameter = 60;
-    setObjectCollider(temp, spriteTypes.ENEMY, true);
+function spawnEnemy(num){
+    let temp = new Enemy(num);
     enemyList.push(temp);
 }
 
 //Similar to spawnEnemy() but this takes a pair of x,y cords and spawns it at the location
-function spawnEnemyAt(x, y){
-    let temp = new Sprite(x, y, 25, 25);
-    setObjectCollider(temp, spriteTypes.ENEMY, true);
+function spawnEnemyAt(num, x, y){
+    let temp = new Enemy(num, x, y);
     enemyList.push(temp);
 }
 
 //Removes latest spawned enemy
 function removeEnemy(){
     if(enemyList.length > 0){
-        enemyList[enemyList.length - 1].remove() 
+        enemyList[enemyList.length - 1].enemySprite.remove() 
         enemyList.pop();
     }
 }
@@ -32,7 +59,11 @@ function removeEnemy(){
 //Spawns an enemy when k key is pressed and removes latest spawed enemy with l key
 function keyPressed(){
     if(keyCode === 75){
-        spawnEnemy();
+        spawnEnemy(0);
+    }
+
+    if(keyCode === 77){
+        spawnEnemy(1);
     }
 
     if(keyCode === 76){
@@ -44,12 +75,32 @@ function keyPressed(){
 function enemyHandler(){
     let previousMax = enemyList.length; //Prevents inf loop if enemey is created while handler is being run
     for(let i = 0; i < previousMax; ++ i) {
-        enemyList[i].moveTowards(player, .005)
+        enemyList[i].enemySprite.moveTowards(player, .005)
 
         //Detects if player and enemy overlaps and changes red if ture, currently need debug mode off to see this
-        if (enemyList[i].overlaps(player)) enemyList[i].color = 'red';
+        if (enemyList[i].enemySprite.overlaps(player)) enemyList[i].enemySprite.color = 'red';
     }
     
+}
+
+//Loads at start
+//Enemy data is harded coded and then pushed onto the staticEnemyList
+function setupStaticEnemeyList(){ //Add new enemies here
+
+    temp = new enemyData(0, 0, "test", 5, 0, 0, 10);
+    staticEnemyList.push(temp);
+
+    temp = new enemyData(1, 0, "test2", 5, 0, 0, 20);
+    staticEnemyList.push(temp);
+
+
+
+    for(let i = 0; i < staticEnemyList.length; ++i){
+        if(i != staticEnemyList[i].enemy_id) {
+            console.log("Enemy id issue at", i);
+            throw new Error("Enemy id's not sequental or number is shared when they should not");
+        }
+    }
 }
 
 
