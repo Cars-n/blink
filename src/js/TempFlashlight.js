d1e3e2577872d2f19c74a36545217abe5cd1bc32
Cars-n <104526149+Cars-n@users.noreply.github.com>
2024-02-26 19:35:46 -0500

    increased darkness layer and lowered opacity

--- src/js/TempFlashlight.js
+++ src/js/TempFlashlight.js
@@ -1,11 +1,11 @@
-const DARKNESSLAYER = 3;
-const DARKNESSOPACITY = 0.01;
-const LIGHTOPACITY = 3.5;
+const DARKNESSLAYER = 10;
+const DARKNESSOPACITY = 0.9;
 
 function darkness() {
       let darkness = new Sprite(width/2, height/2, 1920, 1080); // creates a sprite that is the size of the canvas and at the center of the canvas
       darkness.img = "assets/darkness.svg"
     
+      darkness.opacity = DARKNESSOPACITY;
       darkness.collider = 'none';
       darkness.layer = DARKNESSLAYER; //Layer needs to be higher than player and enviroment sprites
       return darkness;
