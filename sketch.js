2be4b04f088ba995a58435a76e0337b1c08c7686
dsantoli <dsantoli10@outlook.com>
2024-03-04 18:10:07 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,15 +5,20 @@ let playerControl,player,fadeScreen, footsteps, doorCreak;
 let ALL_LOADED=1;
 let notPlayer;
 const PLAYERSPEED = 3;
-let MAINMENULOADED = true;
-let menuScreen;
 let gameMap;
 
+// Main Menu Assets
+// MENU PLAYING or PAUSED
+let GAMESTATE = "MENU";
+let mainMenu;
+let startButton;
+let tutorialButton;
+let controlsButton;
+let quitButton;
 
 function preload() {
 	brickImage = loadImage('./assets/WallRoughDraft.png');
 	floorBoardImage = loadImage("assets/floortiles.png");
-	mainMenuImage = loadImage("assets/Menu_Screen_for_a_pixelated_Horror_game_named_Blink_Set_in_a_Haunted_Mansion.png")
 	doorImage=loadImage("assets/Door.png");
 	darknessImage = loadImage("assets/darkness.svg");
 	soundFormats('mp3');
@@ -21,6 +26,10 @@ function preload() {
 	doorCreak.setVolume(0.5);
 	footsteps = loadSound('assets/audio/footsteps.mp3');
 	footsteps.setVolume(0.5);
+
+	// Main Menu Preload
+	// load background of main menu 
+	mainMenuBackground = loadImage("assets/Main-Menu-Background2.png")
 }
 
 function setup() {
@@ -28,8 +37,6 @@ function setup() {
 	noSmooth(); // removes smoothing to maintain pixelated look
 	canvas.style = ""; // removes default canvas styling
 	fadeScreen = createFadeScreen(); //Creates a screen that's black and fades in and out with the fadeInAndOut function
-	//Creates Room Controller. 
-	menuScreen = createMenuScreen();
 	gameMap=new GameMap();
 	gameMap.render();
 	// roomControl = new RoomController();
@@ -46,17 +53,66 @@ function setup() {
 	//Remove to turn off debug mode
 	// turnOnDebugMode(true, true);
 	
+	// Main Menu Setup
+	//Creates Room Controller. 
+	mainMenu = new Sprite(1920/2, 1080/2, 1920,1080);
+	mainMenu.image = mainMenuBackground;
+	mainMenu.layer = 3;
+	mainMenu.collider = 'none';
+
+	// Setup Start Button
+	startButton = createButton('Start');
+	startButton.position(200,100)
+	startButton.style('background-color', 'transparent'); 
+	startButton.style('color', 'white'); 
+	startButton.style('border', 'none'); 
+	startButton.style('font-size', '25px'); 
+
+	// Setup Tutorial Button
+	tutorialButton = createButton('Tutorial');
+	tutorialButton.position(200,150)
+	tutorialButton.style('background-color', 'transparent'); 
+	tutorialButton.style('color', 'white'); 
+	tutorialButton.style('border', 'none'); 
+	tutorialButton.style('font-size', '25px'); 
+
+	// Setup Controls Button
+	controlsButton = createButton('Controls');
+	controlsButton.position(200,200)
+	controlsButton.style('background-color', 'transparent'); 
+	controlsButton.style('color', 'white'); 
+	controlsButton.style('border', 'none'); 
+	controlsButton.style('font-size', '25px'); 
+
+	// Setup Quit Button
+	quitButton = createButton('Quit');
+	quitButton.position(200,250)
+	quitButton.style('background-color', 'transparent'); 
+	quitButton.style('color', 'white'); 
+	quitButton.style('border', 'none'); 
+	quitButton.style('font-size', '25px'); 
+
 	
 }
 
 function draw() {
 	// console.log("FPS:",1000/deltaTime);
-	clear();
-	if(mouse.presses()){
-		MAINMENULOADED = false;
-		menuScreen.remove();
-	}
-	if(MAINMENULOADED == false){
+	if (GAMESTATE === 'MENU') {
+		// Draw menu
+
+		if(mouse.presses()){
+			GAMESTATE = 'PLAYING';
+
+			// remove 
+			mainMenu.remove();
+			startButton.remove();
+			tutorialButton.remove();
+			controlsButton.remove();
+			quitButton.remove();
+
+		}
+	} else if (GAMESTATE === 'PLAYING') {
+		clear();
 		fadeInAndOut(fadeScreen);
 		movementSounds(player,footsteps);
 		playerMovement.handleInput();
@@ -67,5 +123,18 @@ function draw() {
 		darknessSprite.y = player.y;
 		image(darknessSprite.img, player.x, player.y, darknessSprite.width, darknessSprite.height);
 	}
-
 }
+
+/*
+function createMenuScreen() {
+	// Create a new Sprite object that represents the menu background
+	// Positioned at the center of the screen and spans the full screen size
+	var menu = new Sprite(1920/2, 1080/2, 1920,1080);
+  
+	menu.image = mainMenuBackground;
+	menu.layer = 3;
+	menu.collider = 'none';
+
+	return menu;
+}
+*/
