412ad8be9daf06d1553666e3949ff1354c10bb32
Cars-n <carsonsmith8@hotmail.com>
2024-03-04 12:12:45 -0500

    enemies go behind the darkness and player goes abover
    main menu disabled

--- sketch.js
+++ sketch.js
@@ -5,67 +5,62 @@ let playerControl,player,fadeScreen, footsteps, doorCreak;
 let ALL_LOADED=1;
 let notPlayer;
 const PLAYERSPEED = 3;
-let MAINMENULOADED = true;
+let MAINMENULOADED = false;
 let menuScreen;
 let gameMap;
+let darknessSVG;
 
 
 function preload() {
-	brickImage = loadImage('./assets/WallRoughDraft.png');
-	floorBoardImage = loadImage("assets/floortiles.png");
-	mainMenuImage = loadImage("assets/Menu_Screen_for_a_pixelated_Horror_game_named_Blink_Set_in_a_Haunted_Mansion.png")
-	doorImage=loadImage("assets/Door.png");
-	darknessImage = loadImage("assets/darkness.svg");
-	soundFormats('mp3');
-	doorCreak = loadSound('assets/audio/doorCreak.mp3');
-	doorCreak.setVolume(0.5);
-	footsteps = loadSound('assets/audio/footsteps.mp3');
-	footsteps.setVolume(0.5);
+    brickImage = loadImage('./assets/WallRoughDraft.png');
+    floorBoardImage = loadImage("assets/floortiles.png");
+    mainMenuImage = loadImage("assets/Menu_Screen_for_a_pixelated_Horror_game_named_Blink_Set_in_a_Haunted_Mansion.png")
+    doorImage=loadImage("assets/Door.png");
+    soundFormats('mp3');
+    doorCreak = loadSound('assets/audio/doorCreak.mp3');
+    doorCreak.setVolume(0.5);
+    footsteps = loadSound('assets/audio/footsteps.mp3');
+    footsteps.setVolume(0.5);
+    darknessSVG = loadSVG("assets/darkness.svg");
 }
 
 function setup() {
-	createCanvas(1920,1080,document.getElementById("game"));
-	noSmooth(); // removes smoothing to maintain pixelated look
-	canvas.style = ""; // removes default canvas styling
-	fadeScreen = createFadeScreen(); //Creates a screen that's black and fades in and out with the fadeInAndOut function
-	//Creates Room Controller. 
-	menuScreen = createMenuScreen();
-	gameMap=new GameMap();
-	gameMap.render();
-	// roomControl = new RoomController();
+    createCanvas(1920,1080,document.getElementById("game"));
+    noSmooth(); // removes smoothing to maintain pixelated look
+    canvas.style = ""; // removes default canvas styling
+    fadeScreen = createFadeScreen(); //Creates a screen that's black and fades in and out with the fadeInAndOut function
+    //Creates Room Controller. 
+    menuScreen = createMenuScreen();
+    gameMap=new GameMap();
+    gameMap.render();
+    // roomControl = new RoomController();
 
-	player = setupPlayer();
-	// darkness overlay
-	darknessSprite = darkness();
-	darknessSprite.layer = 0;
-	
-	playerMovement = new MovementController(player,PLAYERSPEED,true);
+    player = setupPlayer();
 
-	setupStaticEnemyList();
-	
-	//Remove to turn off debug mode
-	// turnOnDebugMode(true, true);
+    playerMovement = new MovementController(player,PLAYERSPEED,true);
+
+    setupStaticEnemyList();
+    darknessSetup();
+    //Remove to turn off debug mode
+    // turnOnDebugMode(true, true);
 	
 	
 }
-
 function draw() {
-	// console.log("FPS:",1000/deltaTime);
-	clear();
-	if(mouse.presses()){
-		MAINMENULOADED = false;
+		// console.log("FPS:",1000/deltaTime);
+		clear();
 		menuScreen.remove();
-	}
-	if(MAINMENULOADED == false){
-		fadeInAndOut(fadeScreen);
-		movementSounds(player,footsteps);
-		playerMovement.handleInput();
-		if(kb.presses('o')) spawnEnemyAt(1, player.x - 50, player.y - 50);
-		enemyHandler();
-		darknessSprite.opacity = 0.7;
-		darknessSprite.x = player.x;
-		darknessSprite.y = player.y;
-		image(darknessSprite.img, player.x, player.y, darknessSprite.width, darknessSprite.height);
-	}
+		if(mouse.presses()){
+			MAINMENULOADED = false;
+			menuScreen.remove();
+		}
+		if(MAINMENULOADED == false){
+			fadeInAndOut(fadeScreen);
+			movementSounds(player,footsteps);
+			playerMovement.handleInput();
+			if(kb.presses('o')) spawnEnemyAt(1, player.x - 50, player.y - 50);
+			enemyHandler();
+			
+		}
 
 }
--- src/js/TempFlashlight.js
+++ src/js/TempFlashlight.js
@@ -1,12 +1,28 @@
-const DARKNESSLAYER = 10;
+const DARKNESSLAYER = 2;
 const DARKNESSOPACITY = 0.9;
+let darkness;
 
-function darkness() {
-      let darkness = new Sprite(width/2, height/2, 1920, 1080); // creates a sprite that is the size of the canvas and at the center of the canvas
+function darknessSetup() {
+      darkness = new Sprite(width/2, height/2, 1920, 1080); // creates a sprite that is the size of the canvas and at the center of the canvas
       darkness.img = "assets/darkness.svg"
-    
+
       darkness.opacity = DARKNESSOPACITY;
       darkness.collider = 'none';
       darkness.layer = DARKNESSLAYER; //Layer needs to be higher than player and enviroment sprites
       return darkness;
+}
+
+function updateTriangle(darkSVG) {
+      const triangle = select('#flashlight'); // Select all polygons in the SVG
+      let trianglePoints = extractPoints(triangleMask.attribute('d')); // Get the points of each polygon
+      // Modify the points however you want
+      for (let j = 0; j < points.length; j++) {
+        points[j].x += random(-5, 5); // Example manipulation, adding random value to x coordinate
+        points[j].y += random(-5, 5); // Example manipulation, adding random value to y coordinate
+      }
+}
+
+function darknessDraw(playerx, playery) {
+      const triangle = document.getElementById('cutout');
+      triangle.setAttribute('points', '0,0 1060,740 1060,340');
 }
