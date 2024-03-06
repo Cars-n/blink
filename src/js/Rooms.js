95b871af455c090ea3fef2b739e6881ccb11d460
Cornelius Treadway Pavlic <cornelius.pavlic@gmail.com>
2024-03-06 15:11:49 -0500

    Formatting

--- src/js/Rooms.js
+++ src/js/Rooms.js
@@ -4,42 +4,41 @@ let roomControl;
 let ISWAITING = false;
 
 // Callback function, simply determines whether it is the player ob colliding with door tile or not
-function upDoorCallback(a,b) {
+function upDoorCallback(a, b) {
     if (b?.tag === "player") {
-        if(!ISWAITING){
+        if (!ISWAITING) {
             ISWAITING = true;
             fadeScreenNow();
             playerMovement.moveSpeed = 0;
-            player.room['y']-=1
-            gameMap.loadRoom(player.room['x'],player.room['y']);
-            if(doorCreak.isPlaying() == false) doorCreak.play();
+            player.room["y"] -= 1;
+            gameMap.loadRoom(player.room["x"], player.room["y"]);
+            if (doorCreak.isPlaying() == false) doorCreak.play();
             waitForOpacityCondition(5000) // Wait for up to 5 seconds
-            .then(() => {
-                moveCamera("up");
-                movePlayer("up");
-                ISWAITING = false;
-                playerMovement.moveSpeed = PLAYERSPEED;
-            })
-            .catch((error) => {
-                console.error(error.message);
-            });
+                .then(() => {
+                    moveCamera("up");
+                    movePlayer("up");
+                    ISWAITING = false;
+                    playerMovement.moveSpeed = PLAYERSPEED;
+                })
+                .catch((error) => {
+                    console.error(error.message);
+                });
         }
-    }
-     else {
+    } else {
         console.log("not player: ", b.tag);
     }
-    }
-    
-    function rightDoorCallback(a,b) {
-        if (b?.tag === "player") {
-            if(!ISWAITING){
-                ISWAITING = true;
-                fadeScreenNow();
-                playerMovement.moveSpeed = 0;
-                player.room['x']+=1
-                gameMap.loadRoom(player.room['x'],player.room['y']);
-                if(doorCreak.isPlaying() == false) doorCreak.play();
-                waitForOpacityCondition(5000) // Wait for up to 5 seconds
+}
+
+function rightDoorCallback(a, b) {
+    if (b?.tag === "player") {
+        if (!ISWAITING) {
+            ISWAITING = true;
+            fadeScreenNow();
+            playerMovement.moveSpeed = 0;
+            player.room["x"] += 1;
+            gameMap.loadRoom(player.room["x"], player.room["y"]);
+            if (doorCreak.isPlaying() == false) doorCreak.play();
+            waitForOpacityCondition(5000) // Wait for up to 5 seconds
                 .then(() => {
                     moveCamera("right");
                     movePlayer("right");
@@ -49,80 +48,82 @@ function upDoorCallback(a,b) {
                 .catch((error) => {
                     console.error(error.message);
                 });
-            }
-        } 
-        else {
-            console.log("not player: ", b.tag);
-        }  
-}
-function leftDoorCallback(a,b) {
-    if (b?.tag === "player") {
-    if(!ISWAITING){
-        ISWAITING = true;
-        fadeScreenNow();
-        playerMovement.moveSpeed = 0;
-        player.room['x']-=1
-        gameMap.loadRoom(player.room['x'],player.room['y']);
-        if(doorCreak.isPlaying() == false) doorCreak.play();
-        waitForOpacityCondition(5000) // Wait for up to 5 seconds
-          .then(() => {
-            moveCamera("left");
-            movePlayer("left");
-            ISWAITING = false;
-            playerMovement.moveSpeed = PLAYERSPEED;
-          })
-          .catch((error) => {
-            console.error(error.message);
-          });
         }
-    } 
-    else {
+    } else {
         console.log("not player: ", b.tag);
-    }  
+    }
 }
-function downDoorCallback(a,b) {
+function leftDoorCallback(a, b) {
     if (b?.tag === "player") {
-    if(!ISWAITING){
-        ISWAITING = true;
-        fadeScreenNow();
-        playerMovement.moveSpeed = 0;
-        player.room['y']+=1
-        gameMap.loadRoom(player.room['x'],player.room['y']);
-        if(doorCreak.isPlaying() == false) doorCreak.play();
-        waitForOpacityCondition(5000) // Wait for up to 5 seconds
-          .then(() => {
-            moveCamera("down");
-            movePlayer("down");
-            ISWAITING = false;
-            playerMovement.moveSpeed = PLAYERSPEED;
-          })
-          .catch((error) => {
-            console.error(error.message);
-          });
+        if (!ISWAITING) {
+            ISWAITING = true;
+            fadeScreenNow();
+            playerMovement.moveSpeed = 0;
+            player.room["x"] -= 1;
+            gameMap.loadRoom(player.room["x"], player.room["y"]);
+            if (doorCreak.isPlaying() == false) doorCreak.play();
+            waitForOpacityCondition(5000) // Wait for up to 5 seconds
+                .then(() => {
+                    moveCamera("left");
+                    movePlayer("left");
+                    ISWAITING = false;
+                    playerMovement.moveSpeed = PLAYERSPEED;
+                })
+                .catch((error) => {
+                    console.error(error.message);
+                });
         }
-    } 
-    else {
+    } else {
         console.log("not player: ", b.tag);
-    }  
+    }
+}
+function downDoorCallback(a, b) {
+    if (b?.tag === "player") {
+        if (!ISWAITING) {
+            ISWAITING = true;
+            fadeScreenNow();
+            playerMovement.moveSpeed = 0;
+            player.room["y"] += 1;
+            gameMap.loadRoom(player.room["x"], player.room["y"]);
+            if (doorCreak.isPlaying() == false) doorCreak.play();
+            waitForOpacityCondition(5000) // Wait for up to 5 seconds
+                .then(() => {
+                    moveCamera("down");
+                    movePlayer("down");
+                    ISWAITING = false;
+                    playerMovement.moveSpeed = PLAYERSPEED;
+                })
+                .catch((error) => {
+                    console.error(error.message);
+                });
+        }
+    } else {
+        console.log("not player: ", b.tag);
+    }
 }
 
-class Room{
-    static MAX_T_WIDTH=16;
-    static MAX_T_HEIGHT=9;
+class Room {
+    static MAX_T_WIDTH = 16;
+    static MAX_T_HEIGHT = 9;
 
-    constructor(tWidth,tHeight,tileArray){
-        if((tWidth>Room.MAX_T_WIDTH||tWidth<1)||(tHeight>Room.MAX_T_HEIGHT||tHeight<1)){
-            console.error("Invalid room dimensions!",tWidth,tHeight);
+    constructor(tWidth, tHeight, tileArray) {
+        if (
+            tWidth > Room.MAX_T_WIDTH ||
+            tWidth < 1 ||
+            tHeight > Room.MAX_T_HEIGHT ||
+            tHeight < 1
+        ) {
+            console.error("Invalid room dimensions!", tWidth, tHeight);
             return;
         }
-        this.tWidth=tWidth;
-        this.tHeight=tHeight;
-        this.tileArray=tileArray;
+        this.tWidth = tWidth;
+        this.tHeight = tHeight;
+        this.tileArray = tileArray;
     }
-    getTileArray(){
+    getTileArray() {
         return this.tileArray;
     }
-    getTileSymbol(row,col){
+    getTileSymbol(row, col) {
         return this.tileArray[row][col];
     }
 }
@@ -132,29 +133,67 @@ class RoomController {
     static TILE_HEIGHT = 120;
     static TILE_WIDTH = 120;
     static firstLoad = true;
-	//Defining our tiles that we want to use, keeps them in memory. Not meant to be written to outside of class
-    static wallTile; 
+    //Defining our tiles that we want to use, keeps them in memory. Not meant to be written to outside of class
+    static wallTile;
     static floor;
     static upDoor;
     static rightDoor;
     static leftDoor;
     static downDoor;
 
-
     constructor() {
         // If it's the first load, initialize the tiles
         if (RoomController.firstLoad == true) {
             RoomController.firstLoad == false;
             // Creating wall, floor, and door tiles
-            RoomController.wallTile = new ImageTile(brickImage, '=', RoomController.TILE_WIDTH, RoomController.TILE_HEIGHT, 'static');
-            RoomController.floor = new ImageTile(floorBoardImage, 'o', RoomController.TILE_WIDTH, RoomController.TILE_HEIGHT, 'none',upDoorCallback);
-            RoomController.upDoor = new ImageTile(doorImage, '^', RoomController.TILE_WIDTH, RoomController.TILE_HEIGHT, 'static', upDoorCallback);
-            RoomController.rightDoor = new ImageTile(doorImage, '>', RoomController.TILE_WIDTH, RoomController.TILE_HEIGHT, 'static', rightDoorCallback);
-            RoomController.leftDoor = new ImageTile(doorImage, '<', RoomController.TILE_WIDTH, RoomController.TILE_HEIGHT, 'static', leftDoorCallback);
-            RoomController.downDoor = new ImageTile(doorImage, 'v', RoomController.TILE_WIDTH, RoomController.TILE_HEIGHT, 'static', downDoorCallback);
-			
-
-		}
+            RoomController.wallTile = new ImageTile(
+                brickImage,
+                "=",
+                RoomController.TILE_WIDTH,
+                RoomController.TILE_HEIGHT,
+                "static"
+            );
+            RoomController.floor = new ImageTile(
+                floorBoardImage,
+                "o",
+                RoomController.TILE_WIDTH,
+                RoomController.TILE_HEIGHT,
+                "none",
+                upDoorCallback
+            );
+            RoomController.upDoor = new ImageTile(
+                doorImage,
+                "^",
+                RoomController.TILE_WIDTH,
+                RoomController.TILE_HEIGHT,
+                "static",
+                upDoorCallback
+            );
+            RoomController.rightDoor = new ImageTile(
+                doorImage,
+                ">",
+                RoomController.TILE_WIDTH,
+                RoomController.TILE_HEIGHT,
+                "static",
+                rightDoorCallback
+            );
+            RoomController.leftDoor = new ImageTile(
+                doorImage,
+                "<",
+                RoomController.TILE_WIDTH,
+                RoomController.TILE_HEIGHT,
+                "static",
+                leftDoorCallback
+            );
+            RoomController.downDoor = new ImageTile(
+                doorImage,
+                "v",
+                RoomController.TILE_WIDTH,
+                RoomController.TILE_HEIGHT,
+                "static",
+                downDoorCallback
+            );
+        }
 
         // this.map = [];
     }
@@ -166,28 +205,37 @@ class RoomController {
         const ROOM_WIDTH = RoomController.TILE_WIDTH * 10;
 
         // Rendering rooms and hallway
-        this.renderRoom1(RoomController.TILE_WIDTH/2, RoomController.TILE_HEIGHT/2);
-        this.renderHallway(RoomController.TILE_WIDTH/2 + 1920, RoomController.TILE_HEIGHT/2);
-        this.renderRoom2(RoomController.TILE_WIDTH/2 + (1920 * 2), RoomController.TILE_HEIGHT/2);
+        this.renderRoom1(
+            RoomController.TILE_WIDTH / 2,
+            RoomController.TILE_HEIGHT / 2
+        );
+        this.renderHallway(
+            RoomController.TILE_WIDTH / 2 + 1920,
+            RoomController.TILE_HEIGHT / 2
+        );
+        this.renderRoom2(
+            RoomController.TILE_WIDTH / 2 + 1920 * 2,
+            RoomController.TILE_HEIGHT / 2
+        );
     }
 
     // Method to render a hallway
     renderHallway(x, y) {
         var room = new Tiles(
             [
-
-                '.'.repeat(16),
-                '.'.repeat(16),
-                '.'.repeat(16),
-                '='.repeat(16),
-                '<' + 'o'.repeat(14) + '>',
-                '<' + 'o'.repeat(14) + '>',
-                '='.repeat(16),
-                '.'.repeat(16),
-                '.'.repeat(16),
-                '.'.repeat(16),
+                ".".repeat(16),
+                ".".repeat(16),
+                ".".repeat(16),
+                "=".repeat(16),
+                "<" + "o".repeat(14) + ">",
+                "<" + "o".repeat(14) + ">",
+                "=".repeat(16),
+                ".".repeat(16),
+                ".".repeat(16),
+                ".".repeat(16),
             ],
-            x, y,
+            x,
+            y,
             RoomController.TILE_WIDTH,
             RoomController.TILE_HEIGHT
         );
@@ -197,66 +245,68 @@ class RoomController {
     renderRoom1(x, y) {
         var room = new Tiles(
             [
-                '='.repeat(16),
-                '=' + 'o'.repeat(14) + '=',
-                '=' + 'o'.repeat(14) + '=',
-                '=' + 'o'.repeat(14) + '=',
-                '=' + 'o'.repeat(14) + '>',
-                '=' + 'o'.repeat(14) + '>',
-                '=' + 'o'.repeat(14) + '=',
-                '=' + 'o'.repeat(14) + '=',
-                '='.repeat(16)
+                "=".repeat(16),
+                "=" + "o".repeat(14) + "=",
+                "=" + "o".repeat(14) + "=",
+                "=" + "o".repeat(14) + "=",
+                "=" + "o".repeat(14) + ">",
+                "=" + "o".repeat(14) + ">",
+                "=" + "o".repeat(14) + "=",
+                "=" + "o".repeat(14) + "=",
+                "=".repeat(16),
             ],
-            x, y,
+            x,
+            y,
             RoomController.TILE_WIDTH,
             RoomController.TILE_HEIGHT
         );
     }
-    getRoom1(){
-        var tileMap=[
-            '='.repeat(16),
-            '=' + 'o'.repeat(14) + '=',
-            '=' + 'o'.repeat(14) + '=',
-            '=' + 'o'.repeat(14) + '=',
-            '=' + 'o'.repeat(15),
-            'o' + 'o'.repeat(15),
-            '=' + 'o'.repeat(14) + '=',
-            '=' + 'o'.repeat(14) + '=',
-            '='.repeat(16)
-        ]
-        var room=new Room(16,9,tileMap)
+    getRoom1() {
+        var tileMap = [
+            "=".repeat(16),
+            "=" + "o".repeat(14) + "=",
+            "=" + "o".repeat(14) + "=",
+            "=" + "o".repeat(14) + "=",
+            "=" + "o".repeat(15),
+            "o" + "o".repeat(15),
+            "=" + "o".repeat(14) + "=",
+            "=" + "o".repeat(14) + "=",
+            "=".repeat(16),
+        ];
+        var room = new Room(16, 9, tileMap);
         return room;
     }
-    getConnectorRoom(){
-        var tileMap=[
-            '='.repeat(7)+"^^"+'='.repeat(7),
-            '=' + 'o'.repeat(14) + '=',
-            '=' + 'o'.repeat(14) + '=',
-            '=' + 'o'.repeat(14) + '=',
-            '<' + 'o'.repeat(14) + '>',
-            '<' + 'o'.repeat(14) + '>',
-            '=' + 'o'.repeat(14) + '=',
-            '=' + 'o'.repeat(14) + '=',
-            '='.repeat(7)+"vv"+'='.repeat(7)
+    getConnectorRoom() {
+        var tileMap = [
+            "=".repeat(7) + "^^" + "=".repeat(7),
+            "=" + "o".repeat(14) + "=",
+            "=" + "o".repeat(14) + "=",
+            "=" + "o".repeat(14) + "=",
+            "<" + "o".repeat(14) + ">",
+            "<" + "o".repeat(14) + ">",
+            "=" + "o".repeat(14) + "=",
+            "=" + "o".repeat(14) + "=",
+            "=".repeat(7) + "vv" + "=".repeat(7),
         ];
-        var room=new Room(16,9,tileMap);
+        var room = new Room(16, 9, tileMap);
         return room;
     }
     // Method to render room 2
     renderRoom2(x, y) {
         var room = new Tiles(
             [
-                '='.repeat(16),
-                '=' + 'o'.repeat(14) + '=',
-                '=' + 'o'.repeat(14) + '=',
-                '=' + 'o'.repeat(14) + '=',
-                '<' + 'o'.repeat(14) + '=',
-                '<' + 'o'.repeat(14) + '=',
-                '=' + 'o'.repeat(14) + '=',
-                '=' + 'o'.repeat(14) + '=',
-                '='.repeat(16)
+                "=".repeat(16),
+                "=" + "o".repeat(14) + "=",
+                "=" + "o".repeat(14) + "=",
+                "=" + "o".repeat(14) + "=",
+                "<" + "o".repeat(14) + "=",
+                "<" + "o".repeat(14) + "=",
+                "=" + "o".repeat(14) + "=",
+                "=" + "o".repeat(14) + "=",
+                "=".repeat(16),
             ],
-            x, y,
+            x,
+            y,
             RoomController.TILE_WIDTH,
             RoomController.TILE_HEIGHT
         );
--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,28 @@
 function createMenuScreen() {
-  var object = new Sprite(1920/2, 1080/2, 1920,1080);
-  object.image = mainMenuImage;
-  object.layer = 10;
-  object.collider = 'none';
-  return object;
+    var object = new Sprite(1920 / 2, 1080 / 2, 1920, 1080);
+    object.image = mainMenuImage;
+    object.layer = 10;
+    object.collider = "none";
+    return object;
 }
 
 function waitForOpacityCondition(timeout) {
     return new Promise((resolve, reject) => {
-      const startTime = Date.now();
-  
-      function checkCondition() {
-        // console.log("this is the Opacity: " + OPACITYEQUALSONE + "\n"+ "this is the variable: " + variable);
-        if (OPACITYEQUALSONE === true) {
-          resolve(OPACITYEQUALSONE);
-        } else if (Date.now() - startTime >= timeout) {
-          reject(new Error('Timeout waiting for condition'));
-        } else {
-          setTimeout(checkCondition, 1); // Adjust the interval as needed
-        }
-      }
-  
-      checkCondition();
-    });
-  }
-  
-  // Example usage
+        const startTime = Date.now();
 
-  
+        function checkCondition() {
+            // console.log("this is the Opacity: " + OPACITYEQUALSONE + "\n"+ "this is the variable: " + variable);
+            if (OPACITYEQUALSONE === true) {
+                resolve(OPACITYEQUALSONE);
+            } else if (Date.now() - startTime >= timeout) {
+                reject(new Error("Timeout waiting for condition"));
+            } else {
+                setTimeout(checkCondition, 1); // Adjust the interval as needed
+            }
+        }
+
+        checkCondition();
+    });
+}
+
+// Example usage
