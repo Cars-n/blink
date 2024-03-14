c92eb58d7d9b4aa1123141e69641628d6e6b3b5c
dsantoli <dsantoli10@outlook.com>
2024-03-14 15:28:04 -0400

    changing style of buttons / color scheme - added css styling for main - menu

--- src/js/Screens.js
+++ src/js/Screens.js
@@ -223,55 +223,46 @@ class PauseMenu {
 
 }
 
+/**
+ * Class to handle the control of the main menu and actions in the menu
+ */
 class MainMenu {
 	/**
 	 * Default constructor, makes a background, resume and exit buttons
 	 */
 	constructor() {
-		this.startButton = createButton('Start');
-		this.tutorialButton = createButton('Tutorial')
-		this.controlsButton = createButton('Controls')
-		this.exitButton = createButton('Exit');
+		this.startButton = createButton('> Start');
+		this.tutorialButton = createButton('> Tutorial')
+		this.controlsButton = createButton('> Controls')
+		this.exitButton = createButton('> Exit');
 
 		//Backdrop to the menu
 		this.menu = new Sprite(1920/2,1080/2,1920,1080);
 		this.menu.layer = MAIN_MENU_LAYER;
 		this.menu.opacity = 0.4;
 		this.menu.color = 'black';
-		this.menu.image = mainMenuBackground;
+		// this.menu.image = mainMenuBackground;
 		this.menu.collider = 'none';
 		
 
 		//Setting up the start button
+		this.startButton.class("MainMenuButtons");
 		this.startButton.position(150, 100)
-		this.startButton.style('background-color', 'transparent'); 
-		this.startButton.style('color', 'white'); 
-		this.startButton.style('border', 'none'); 
-		this.startButton.style('font-size', '25px');
 		this.startButton.hide();		//Hides the button until pause menu is triggered
 
 		//Setting up the tutorial button
+		this.tutorialButton.class("MainMenuButtons");
 		this.tutorialButton.position(150, 150)
-		this.tutorialButton.style('background-color', 'transparent'); 
-		this.tutorialButton.style('color', 'white'); 
-		this.tutorialButton.style('border', 'none'); 
-		this.tutorialButton.style('font-size', '25px');
 		this.tutorialButton.hide();		//Hides the button until pause menu is triggered
 
 		//Setting up the controls button
+		this.controlsButton.class("MainMenuButtons");
 		this.controlsButton.position(150, 200)
-		this.controlsButton.style('background-color', 'transparent'); 
-		this.controlsButton.style('color', 'white'); 
-		this.controlsButton.style('border', 'none'); 
-		this.controlsButton.style('font-size', '25px');
 		this.controlsButton.hide();		//Hides the button until pause menu is triggered
 
 		// Setup exit Button
+		this.exitButton.class("MainMenuButtons");
 		this.exitButton.position(150,250)
-		this.exitButton.style('background-color', 'transparent'); 
-		this.exitButton.style('color', 'white'); 
-		this.exitButton.style('border', 'none'); 
-		this.exitButton.style('font-size', '25px');
 		this.exitButton.hide();		//Hides the button until pause menu is triggered
 	}
 
