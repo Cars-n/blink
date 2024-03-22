fc452a8a8eac1ec3d44c81f880cd28da500703d9
Cars-n <carsonsmith8@hotmail.com>
2024-03-22 00:36:01 -0400

    added smooth transitions

--- src/js/Flashlight.js
+++ src/js/Flashlight.js
@@ -1,6 +1,7 @@
 const DARKNESSLAYER = 2;
-const ROTATION_SPEED = 60;
-let darkness, targetRotation, clockwiseAmt, counterClockwiseAmt;
+const ROTATION_SPEED = 0.1;
+let darkness, clockwiseAmt, counterClockwiseAmt
+let currentRotation = 0, targetRotation = 0;
 
 
 function darknessSetup() {
@@ -15,6 +16,10 @@ function darknessDraw(playerX, playerY, playerVelocityX, playerVelocityY) {
       darkness.y = playerY;
       darkness.x = playerX;
       if(playerVelocityX != 0 || playerVelocityY != 0) { // if player is moving
-            darkness.rotation = (atan2(playerVelocityX, -playerVelocityY) + 360) % 360; // calculates the angle formed between two points
+            targetRotation = (atan2(playerVelocityX, -playerVelocityY) + 360) % 360; // calculates the angle formed between two points
       }
+
+      currentRotation = lerp(currentRotation, targetRotation, ROTATION_SPEED); // currentRotation gets ROTATION_SPEED times closer to the target
+
+      darkness.rotation = currentRotation;
 }
