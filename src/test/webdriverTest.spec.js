89dadb87e03b0e8f05178f49dc5c4a7ff960235c
Elliott Hager <ehager2@kent.edu>
2024-04-03 16:25:40 -0400

    Currently have Python, Webdriver, Node, NPM, WGET, Chrome, Chrome Driver, and a Python web server installed and running to run tests.
    Still running into issues with the chromedriver being found. Working to reconfigure the action to point directly to the driver

--- src/test/webdriverTest.spec.js
+++ src/test/webdriverTest.spec.js
@@ -1,7 +1,9 @@
-const {By, Builder, Browser} = require('selenium-webdriver');
+const {By, Builder, Browser, ChromiumWebDriver, Options} = require('selenium-webdriver');
 const assert = require("assert");
 const { fileURLToPath } = require('url');
 const { time } = require('console');
+
+require("chromedriver");
 
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
@@ -9,28 +11,33 @@ async function testStartButton() {
     let driver;
     
     try {
-      driver = await new Builder().forBrowser(Browser.CHROME).build();
+      
+      //Configuring the options of the server
+      //options = new ChromeOptions();
+      //options.addArguments("--headless"); //Makes chrome launch headless for non-GUI use
+
+      driver = new Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
 
       //await driver.get();
 
       console.log("Grabbing the local game instance");
-      await driver.get("https://cars-n.github.io/blink/"); //grabs the page
+      await driver.get("http://localhost:8000"); //grabs the page
       console.log("Got the local instance!");
 
       //await sleep(10000);
 
-      //Now grabs the start button and sends click to play
-      console.log("Grabing the start button by name");
+      // //Now grabs the start button and sends click to play
+      // console.log("Grabing the start button by name");
       
-      let startButton = await driver.findElement(By.name("start"));
-      console.log("Got the start button by name");
+      // let startButton = await driver.findElement(By.name("start"));
+      // console.log("Got the start button by name");
       
-      console.log("Clicking the start button to start the game");
-      //await startButton.click();
-      console.log("Clicked the start button, game is started");
+      // console.log("Clicking the start button to start the game");
+      // //await startButton.click();
+      // console.log("Clicked the start button, game is started");
     
-      console.log("Checking visibility of the start button");
-      //assert(startButton.getAttribute("visible"));
+      // console.log("Checking visibility of the start button");
+      // //assert(startButton.getAttribute("visible"));
 
 
     }  catch (e) {
