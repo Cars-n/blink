73781e657a059b226de793ff9af48c5be36532bb
Chris-G32 <cgruska@kent.edu>
2024-04-05 21:07:38 -0400

    Test for 404 errors with puppeteer

---
+++ tests/test-no404.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+const core=require('@actions/core')
+async function monitorNetwork() {
+    const browser = await puppeteer.launch({headless:true,args: ['--no-sandbox']});
+    const page = await browser.newPage();
+    var count404=0;
+    // Listen for all network requests
+
+    page.on('response', async response => {
+        if(response.status()==404){
+            console.error("Request failed for resource at: ",response.url());
+            count404+=1;
+        }
+    });
+    await page.goto('http://127.0.0.1:5500/index.html');
+    await browser.close();
+    return count404;
+}
+monitorNetwork().then((errorCount)=>{
+    if(errorCount>0){
+        core.setFailed("404 Error(s) Detected");
+    }
+});
+
