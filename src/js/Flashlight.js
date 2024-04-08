155596ef84bb9fbb079fbc84971ea8b955d4f9b5
Cars-n <carsonsmith8@hotmail.com>
2024-04-08 18:06:59 -0400

    made flashlight bigger

--- src/js/Flashlight.js
+++ src/js/Flashlight.js
@@ -5,7 +5,6 @@ let flashlightRotation;
 function darknessSetup() {
       darkness = new Sprite(width/2, height/2, 1920, 1080); // creates a sprite that is the size of the canvas and at the center of the canvas
       darkness.img = "assets/darkness.svg"
-      darkness.opacity = 0.4;
       darkness.collider = 'none';
       darkness.layer = DARKNESS_LAYER; // layer needs to be higher than player and enviroment sprites
 } 
