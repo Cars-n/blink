ec3b6b6506275e54efbf237b9b0fa13d90aca1d1
Elliott Hager <ehager2@kent.edu>
2024-04-13 12:57:07 -0400

    Adding in player setup function tests and testing setting up the bullets

--- sketch.js
+++ sketch.js
@@ -27,6 +27,10 @@ let CreepyPiano2;
 let trapDoorImage;
 let cellBarsImage;
 let bullets;
+
+//Testing
+//let testPlayer = setupPlayer();
+
 //needs to be false when game is ready to play, is false for testing.
 console.log("FIX THIS VALUE");
 let ENEMY42SPAWED = true;
--- test/testPlayer.js
+++ test/testPlayer.js
@@ -1,10 +1,28 @@
-var playerTest = setupPlayer();
+const assert = require('assert');
+const { expect } = require('chai');
 
 function playerCreation() {
+    let testPlayer = playerSetup();
+
     describe("Player", function() {
         describe("#Constructing a new player", function() {
             it('should be of type Sprite');
-            expect(playerTest).to.be.a('Sprite');
+            expect(testPlayer).to.be.type('Sprite');
+
+            it('should have a colider of dynamic to interact with the world');
+            expect(testPlayer.collide()).to.equal('dynamic');
+
+            it ('should have 101 health');
+            expect(testPlayer.health).to.equal(101);
+        });
+
+        describe("#Setting up bullets for gun to attack", function () {
+            setUpBullets();
+
+            it("should look like a bullet");
+            expect(bullets.image).to.equal("assets/bullet.png");
+
+            
         });
 
     });
