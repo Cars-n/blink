aaa6f534ec69c19821168516af5e0a1fca24a4c4
Cars-n <104526149+Cars-n@users.noreply.github.com>
2024-04-15 17:00:10 -0400

    added actionchains from selenium

--- tests/selenium/webdriver.py
+++ tests/selenium/webdriver.py
@@ -3,7 +3,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
+
 # from webdriver_manager.chrome import ChromeDriverManager
 import time
 import chromedriver_autoinstaller
@@ -18,15 +20,13 @@ options = [
   # Define window size here
     "--ignore-certificate-errors",
     "--headless",
-    #"--disable-gpu",
-    #"--window-size=1920,1200",
-    #"--ignore-certificate-errors",
-    #"--disable-extensions",
+    # "--disable-gpu",
+    # "--window-size=1920,1200",
+    # "--ignore-certificate-errors",
+    # "--disable-extensions",
     "--no-sandbox"
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
+    # "--disable-dev-shm-usage",
+    # '--remote-debugging-port=9222'
 ]
 
 for option in options:
@@ -44,7 +44,7 @@ def test_wikipedia_python_results():
     search_input.send_keys(query)  
     search_input.send_keys(Keys.RETURN)  
     assert "Guido" in browser.page_source
-    time.sleep(10)
+    
     browser.close()
 
 def test_wikipedia_CPP_results():
@@ -57,7 +57,7 @@ def test_wikipedia_CPP_results():
     search_input.send_keys(query)  
     search_input.send_keys(Keys.RETURN)  
     assert "Bjarne Stroustrup" in browser.page_source
-    time.sleep(3)
+    
     browser.close()
 def open_browser(link='http://127.0.0.1:8000/index.html'):
     browser = webdriver.Chrome(options = chrome_options)
@@ -136,7 +136,6 @@ def test_movement():
     assert new_y_pos > y_pos, "Failed to move downwards"
     print("Downward movement passed")
 
-
     print("Testing player can move to the left...")
     x_pos = browser.execute_script('return player.x;')
     print("Player X position: ", x_pos)
@@ -152,7 +151,6 @@ def test_movement():
     print("Player X position after moving to the left: ", new_x_pos)
     assert new_x_pos < x_pos, "Failed to move to the left"
     print("Left movement passed")
-
 
     print("Testing player can move to the right...")
     x_pos = browser.execute_script('return player.x;')
