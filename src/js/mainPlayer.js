3edbe85a47dbb80363a4b7fd1a9358054271b56e
Elliott Hager <ehager2@kent.edu>
2024-04-14 20:46:49 -0400

    Trying the type of module

--- src/js/mainPlayer.js
+++ src/js/mainPlayer.js
@@ -1,8 +1,8 @@
 if (typeof module !== 'undefined' && module.exports) { 
     console.log('this script is running in Node.js'); 
     const Room = import('../src/js/Rooms.js');
-    await import('../src/js/Rooms.js');
-    const RoomController = Room.RoomController;
+    
+    import { RoomController } from '../js/Rooms.js';
 
 } else { 
     console.log('this script is not running in Node.js'); 
