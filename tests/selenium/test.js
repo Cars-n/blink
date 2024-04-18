0f60fac8d3bb25159ca890c46548bfb77cfea9ae
Elliott Hager <ehager2@kent.edu>
2024-04-18 17:49:38 -0400

    Commenting some of the tests out to see where things are getting stuck

--- tests/selenium/test.js
+++ tests/selenium/test.js
@@ -7,24 +7,24 @@ function playerCreation() {
 
     describe("Player", function() {
         describe("#Constructing a new player", function() {
-            it('should be of type Sprite');
-            expect(testPlayer).to.be.type('Sprite');
+            // it('should be of type Sprite');
+            // expect(testPlayer).to.be.type('Sprite');
 
-            it('should have a colider of dynamic to interact with the world');
-            expect(testPlayer.collide()).to.equal('dynamic');
+            // it('should have a colider of dynamic to interact with the world');
+            // expect(testPlayer.collide()).to.equal('dynamic');
 
-            it ('should have 101 health');
-            expect(testPlayer.health).to.equal(101);
+            // it ('should have 101 health');
+            // expect(testPlayer.health).to.equal(101);
         });
 
-        describe("#Setting up bullets for gun to attack", function () {
-            setUpBullets();
+        // describe("#Setting up bullets for gun to attack", function () {
+        //     setUpBullets();
 
-            it("should look like a bullet");
-            expect(bullets.image).to.equal("assets/bullet.png");
+        //     it("should look like a bullet");
+        //     expect(bullets.image).to.equal("assets/bullet.png");
 
             
-        });
+        // });
 
     });
 }
