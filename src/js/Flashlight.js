50155c4da415974488cc404a46c64dd391dff381
Cars-n <104526149+Cars-n@users.noreply.github.com>
2024-04-24 19:25:42 -0400

    fixed darkness_layer constant variable name

--- src/js/Flashlight.js
+++ src/js/Flashlight.js
@@ -7,7 +7,7 @@ function darknessSetup() {
       darkness = new Sprite(width/2, height/2, 1920, 1080); // creates a sprite that is the size of the canvas and at the center of the canvas
       darkness.img = "assets/darknessEyesight.svg"
       darkness.collider = 'none';
-      darkness.layer = DARKNESSLAYER; // layer needs to be higher than player and enviroment sprites
+      darkness.layer = DARKNESS_LAYER; // layer needs to be higher than player and enviroment sprites
 } 
 
 function darknessDraw(playerX, playerY, playerVelocityX, playerVelocityY, hasFlashlight) {     
