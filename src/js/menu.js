52706a380b660b5c155871e1bb2e7d9c754fe7ff
Cornelius Treadway Pavlic <cornelius.pavlic@gmail.com>
2024-04-29 13:17:55 -0400

    Revert "Created a new screen where the player can adjust the blinking sensitivity."
    This reverts commit af79eb2dee4795de54f6294918e7b0d79f1ca8b1.

---
+++ src/js/menu.js
@@ -0,0 +1,39 @@
+function menuFunctionality(){
+    if(!mainMenuSound.isPlaying()) mainMenuSound.play();
+
+		player.velocity.y = 0;
+		player.velocity.x = 0;
+		player.visible = false;
+		healthBar.visible = false;
+		player.changeAni("idle_" + playerMovement.lastDirection);
+		movementSounds(player,footsteps);
+
+		mainMenu.showMenu();
+
+		mainMenu.startButton.mousePressed(() => {
+			player.visible = true;
+			healthBar.visible = true;
+			moveCamera("right",SPAWNX);
+			moveCamera("down",SPAWNY);
+			var coords=gameMap.getRoomWorldCoords(SPAWNX,SPAWNY);
+			player.x=coords.x+600;
+			player.y=coords.y+600;
+			player.room["x"]=SPAWNX;
+			player.room["y"]=SPAWNY;
+			fadeScreen.x = player.x;
+			fadeScreen.y = player.y;
+			gameMap.loadRoom(SPAWNX,SPAWNY);
+			mainMenuSound.pause();
+			GAMESTATE = mainMenu.startGame(GAMESTATE);
+		});
+
+		mainMenu.exitButton.mousePressed(() => {		
+			/* TODO - LEFT OPEN FOR THE MAIN MENU METHODS TO DISPLAY */
+			alert("What, got too scared and quit?");
+		});
+		
+		if(kb.pressed('l')){
+			
+			GAMESTATE = mainMenu.startGame(GAMESTATE);		
+		} 
+}
